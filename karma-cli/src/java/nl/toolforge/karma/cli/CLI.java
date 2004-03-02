4a327ce8bfe2d15b726be31de5f17a9c89e80f34
Hilbert Schraal <hilbert@hippe-it.nl>
2004-03-02 23:15:12 +0000

    *** empty log message ***

--- karma-cli/src/java/nl/toolforge/karma/cli/CLI.java
+++ karma-cli/src/java/nl/toolforge/karma/cli/CLI.java
@@ -6,6 +6,7 @@ import nl.toolforge.karma.core.cmd.CommandContext;
 import nl.toolforge.karma.core.cmd.CommandDescriptor;
 import nl.toolforge.karma.core.cmd.CommandResponse;
 import nl.toolforge.karma.core.cmd.CommandMessage;
+import nl.toolforge.karma.core.cmd.CommandFactory;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -28,151 +29,126 @@ import java.util.ResourceBundle;
  */
 public class CLI {
 
-	private static final ResourceBundle FRONTEND_MESSAGES = BundleCache.FRONTEND_MESSAGES;
+    private static final ResourceBundle FRONTEND_MESSAGES = BundleCache.FRONTEND_MESSAGES;
 
-	private static Log logger = LogFactory.getLog(CLI.class);
+    private static Log logger = LogFactory.getLog(CLI.class);
 
 //	private static Preferences prefs = Preferences.getInstance();
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		CLI cli = new CLI();
+        CLI cli = new CLI();
 
 
-		ConsoleWriter writer = new ConsoleWriter(true);
+        ConsoleWriter writer = new ConsoleWriter(true);
 
-		// Initialize the command context
-		//
-		CommandContext ctx = new CommandContext();
-		try {
-			ctx.init();
-		} catch (KarmaException k) {
+        // Initialize the command context
+        //
+        CommandContext ctx = new CommandContext();
+        try {
+            ctx.init();
+        } catch (KarmaException k) {
 
-      writer.writeln(k.getErrorMessage());
+            writer.writeln(k.getErrorMessage());
 
-			logger.error(k.getMessage());
+            logger.error(k.getMessage());
 
-      writer.writeln(FRONTEND_MESSAGES.getString("message.EXIT"));
-			System.exit(1);
-		}
+            writer.writeln(FRONTEND_MESSAGES.getString("message.EXIT"));
+            System.exit(1);
+        }
 
-		try {
+        try {
 
-      // Open a reader, which is the actual command line ...
-      //
-      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            // Open a reader, which is the actual command line ...
+            //
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-			while (true) {
+            while (true) {
 
-				writer.prompt();
+                writer.prompt();
 
-				String line = reader.readLine().trim();
+                String line = reader.readLine().trim();
 
-				if ((line == null) || ("".equals(line.trim()))) {
-					continue;
-				}
+                if ((line == null) || ("".equals(line.trim()))) {
+                    continue;
+                }
 
-				String commandName = null;
-				String options = "";
 
-				// Check if the user wants to exit
-				//
-				if (ConsoleConfiguration.getExitCommands().contains(line.trim().toUpperCase())) {
+                // Check if the user wants to exit
+                //
+                if (ConsoleConfiguration.getExitCommands().contains(line.trim().toUpperCase())) {
 
-					String text = FRONTEND_MESSAGES.getString("message.THANK_YOU");
-					int length = text.length();
+                    String text = FRONTEND_MESSAGES.getString("message.THANK_YOU");
+                    int length = text.length();
 
-					StringBuffer g = new StringBuffer();
-					g.append("\n\n").append(StringUtils.repeat("*", length));
-					g.append("\n").append(text).append("\n");
-					g.append(StringUtils.repeat("*", length)).append("\n");
+                    StringBuffer g = new StringBuffer();
+                    g.append("\n\n").append(StringUtils.repeat("*", length));
+                    g.append("\n").append(text).append("\n");
+                    g.append(StringUtils.repeat("*", length)).append("\n");
 
-					writer.writeln(g.toString());
+                    writer.writeln(g.toString());
 
-					break;
-				}
+                    break;
+                }
 
-				// Filter out the HELP command
-				//
-				if (line.trim().toLowerCase().startsWith("help") || line.trim().startsWith("?")) {
+                // Filter out the HELP command
+                //
+                try {
+                    if (line.trim().toLowerCase().startsWith("help") || line.trim().startsWith("?")) {
 
-					writer.writeln(FRONTEND_MESSAGES.getString("message.VALID_COMMANDS"));
+                        writer.writeln(FRONTEND_MESSAGES.getString("message.VALID_COMMANDS"));
 
-					String[] commandStrings = cli.formatCommands(ctx.getCommands().values());
-					for (int i = 0; i < commandStrings.length; i++) {
-						writer.writeln(commandStrings[i]);
-					}
+                        String[] commandStrings = cli.formatCommands(CommandFactory.getInstance().getCommands());
+                        for (int i = 0; i < commandStrings.length; i++) {
+                            writer.writeln(commandStrings[i]);
+                        }
 
-				} else {
+                    } else {
+                        CommandResponse response = ctx.execute(line);
 
-					// Other commands ...
-					//
-					if (line.indexOf(" ") > 0) {
-						commandName = line.substring(0, line.indexOf(" "));
-					} else {
-						commandName = line.substring(0);
-					}
+                        // For now, just print the response.
+                        //
+                        CommandMessage[] messages = response.getMessages();
 
-					// Check if the command is a valid command
-					//
-					if (ctx.isCommand(commandName)) {
+                        // Print the first message for now.
+                        // TODO do something better with the message array
+                        //
+                        writer.writeln(messages[0].getMessageText());
 
-						if (line.indexOf(" ") > 0) {
-							options = line.substring(line.indexOf(" ") + 1);
-						}
+                    }
+                } catch (KarmaException e) {
+                    writer.writeln(e.getErrorMessage());
+                }
+            }
+            writer.writeln(FRONTEND_MESSAGES.getString("message.EXIT"));
+            logger.info("Exiting Karma ...");
+            System.exit(0);
+        }
+        catch (IOException e) {
+            logger.error(e.getMessage(), e);
+            System.exit(1);
+        }
+    }
 
-						try {
-							CommandResponse response = ctx.execute(commandName, options);
+    private String[] formatCommands(Collection commands) {
 
-              // For now, just print the response.
-              //
-              CommandMessage[] messages = response.getMessages();
+        String[] commandStrings = new String[commands.size()];
 
-              // Print the first message for now.
-              // TODO do something better with the message array
-              //
-              writer.writeln(messages[0].getMessageText());
+        int j = 0;
 
-						} catch (KarmaException a) {
-							writer.writeln(a.getErrorMessage());
-						}
+        for (Iterator i = commands.iterator(); i.hasNext();) {
+            CommandDescriptor descriptor = (CommandDescriptor) i.next();
 
-					} else {
-
-						writer.writeln(commandName.trim().concat(" " + FRONTEND_MESSAGES.getString("message.INVALID_COMMAND")));
-					}
-
-				}
-			}
-      writer.writeln(FRONTEND_MESSAGES.getString("message.EXIT"));
-			logger.info("Exiting Karma ...");
-			System.exit(0);
-		}
-		catch (IOException e) {
-			logger.error(e.getMessage(), e);
-			System.exit(1);
-		}
-	}
-
-	private String[] formatCommands(Collection commands) {
-
-		String[] commandStrings = new String[commands.size()];
-
-		int j = 0;
-
-		for (Iterator i = commands.iterator(); i.hasNext();) {
-			CommandDescriptor descriptor = (CommandDescriptor) i.next();
-
-			int count1 = 25 - (descriptor.getName().length() + descriptor.getAlias().length() + 2);
+            int count1 = 25 - (descriptor.getName().length() + descriptor.getAlias().length() + 2);
 //      logger.debug(">> count : " + count1);
 
-			commandStrings[j++] =
-				descriptor.getName() + "(" + descriptor.getAlias() + ")" +
-				StringUtils.repeat(" ", count1) + descriptor.getDescription();
+            commandStrings[j++] =
+                    descriptor.getName() + "(" + descriptor.getAlias() + ")" +
+                    StringUtils.repeat(" ", count1) + descriptor.getDescription();
 //			logger.debug(">> String: " + commandStrings[j-1]);
-		}
+        }
 
-		return commandStrings;
+        return commandStrings;
 
-	}
+    }
 }
---
+++ karma-cli/src/java/nl/toolforge/karma/cli/cmd/SelectManifestImpl.java
@@ -0,0 +1,16 @@
+package nl.toolforge.karma.cli.cmd;
+
+import nl.toolforge.karma.core.cmd.impl.SelectManifest;
+import nl.toolforge.karma.core.cmd.CommandResponse;
+import nl.toolforge.karma.core.KarmaException;
+
+/**
+ * @author W.H. Schraal
+ *
+ * @version $Id$
+ */
+public class SelectManifestImpl extends SelectManifest {
+    public CommandResponse execute() throws KarmaException {
+        return null;
+    }
+}
