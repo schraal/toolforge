c6082d6e426b2bca5120445f7274c48fde1ab12d
Hilbert Schraal <hilbert@hippe-it.nl>
2004-03-31 06:37:37 +0000

    writing documentation after bedtime is no fun.

--- karma-core/src/java/nl/toolforge/karma/core/LocalEnvironment.java
+++ karma-core/src/java/nl/toolforge/karma/core/LocalEnvironment.java
@@ -13,57 +13,75 @@ import java.util.Properties;
 
 /**
  * This class maintains a users' local environment. The local environment is required to properly run Karma tools. It
- * contains references to directories and handles history settings.
+ * contains references to directories, which are necessary to bootstrap Karma.
  *
- * The environment consists of two parts:
+ * LocalEnvironment can be instanciated in two ways:
  *
- * 1/ bootstrap configuration which is stored in the $USER_HOME/.karma/karma.properties
- * 2/ dynamic configuration which is stored using java.util.prefs
+ * <ol>
+ *   <li/>Pass a Properties Object to the getInstance() method. The properties are read from this Object.
+ *   <li/>Don't pass anything to the getInstance() method. The properties are read from file. The properties are
+ *        assumed to be located in $USER_HOME/.karma/karma.properties.
+ * </ol>
+ *
+ * This class won't be instanciated when the necessary properties can't be found, or reference non-existing,
+ * non-createable directories.
  *
  * @author D.A. Smedes
+ * @author W.H. Schraal
  *
- * @version $Id:
+ * @version $Id$
  */
 public class LocalEnvironment {
 
   private static Log logger = LogFactory.getLog(LocalEnvironment.class);
 
-  /** Determines testmodes and disables file access */
-//  private static final boolean TESTMODE = (System.getProperty("TESTMODE") == null ? false : System.getProperty("TESTMODE").equals("true"));
-
-  /**
-   * This property is used to determine the path to the bootstrap properties file (<code>karma.properties</code>). The
-   * convention is that a filename <b>NOT</b> starting with a <code>/</code> means that it will be loaded from the
-   * classpath, otherwise the value for the property will be considered an absolute filename.
-   */
-  public static final String BOOTSTRAP_CONFIGURATION_DIRECTORY = "bootstrap.configuration";
-
-
 
   /** Property that identifies the user's development home directory. */
   public static final String DEVELOPMENT_HOME_DIRECTORY = "development.store";
+
   /** Property that identifies the directory where manifest files are stored. */
   public static final String MANIFEST_STORE_DIRECTORY = "manifest.store";
+
   /** Property that identifies the directory where location files are stored. */
   public static final String LOCATION_STORE_DIRECTORY = "location.store";
 
 
+  /** Property Object that stores the key-value pairs of the defined properties. */
   private Properties configuration = new Properties();
 
 
+  /** The one-and-only instance of this class. */
   private static LocalEnvironment localEnvironment;
 
+  /**
+   * Return the one-and-only instance of this class. It will be initialized from file
+   * when it does not yet exist.
+   */
   public static LocalEnvironment getInstance() {
     return getInstance(null);
   }
 
+  /**
+   * Return the one-and-only instance of this class. It will be initialized using the given
+   * Properties object when it does not yet exist.
+   *
+   * @param properties  Properties object used to initialize the instance.
+   */
   public static LocalEnvironment getInstance(Properties properties) {
     if (localEnvironment == null) {
       localEnvironment = new LocalEnvironment(properties);
+    } else {
+      logger.warn("This singleton class has already been initialized. Use getInstance() instead.");
     }
     return localEnvironment;
   }
 
+  /**
+   * Private constructor. This class is only initialized via the getInstance() methods.
+   *
+   * @param properties  The properties object that is used to initialize. When this parameter is null,
+   *                    the properties are read from file.
+   */
   private LocalEnvironment(Properties properties) {
     try {
 
@@ -127,11 +145,11 @@ public class LocalEnvironment {
 
 
   /**
-   *
-   *
+   * Retrieve a valid reference to the manifest store directory. The available manifests
+   * are stored here.
    *
    * @return A valid reference to the manifest store directory.
-   * @throws ManifestException When the manifest store directory does not exist.
+   * @throws ManifestException  When the manifest store directory does not exist.
    */
   public final File getManifestStore() throws ManifestException {
     try {
@@ -152,9 +170,11 @@ public class LocalEnvironment {
   }
 
   /**
+   * Retrieve a valid reference to the location store directory. The available locations
+   * are stored here.
    *
    * @return A valid reference to the location store directory.
-   * @throws LocationException When then location store directory does not exist.
+   * @throws LocationException  When then location store directory does not exist.
    */
   public final File getLocationStore() throws LocationException {
     try {
@@ -174,7 +194,13 @@ public class LocalEnvironment {
     }
   }
 
-
+  /**
+   * Retrieve a valid reference to the developer home directory. This is the directory where
+   * the manifest instances are checked out from the version control system.
+   *
+   * @return A valid reference to the development home directory.
+   * @throws KarmaException  When the development home does not exists.
+   */
   public final File getDevelopmentHome() throws KarmaException {
     try {
       File home = null;
