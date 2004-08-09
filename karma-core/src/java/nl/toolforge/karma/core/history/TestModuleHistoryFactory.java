37354a683ef878f96e0967768641f71087e1b8eb
Hilbert Schraal <hilbert@hippe-it.nl>
2004-08-09 20:48:43 +0000

    added adding of event

--- karma-core/src/java/nl/toolforge/karma/core/history/TestModuleHistoryFactory.java
+++ karma-core/src/java/nl/toolforge/karma/core/history/TestModuleHistoryFactory.java
@@ -1,30 +1,42 @@
 package nl.toolforge.karma.core.history;
 
 import nl.toolforge.karma.core.test.LocalCVSInitializer;
+import nl.toolforge.karma.core.Version;
 
 import java.io.File;
+import java.util.Date;
 
 /**
  * @author W.H. Schraal
  */
 public class TestModuleHistoryFactory extends LocalCVSInitializer {
 
-    public void testGetHistory() {
-        try {
-            checkoutDefaultModule1();
+  public void testGetSetHistory() {
+      try {
+          checkoutDefaultModule1();
 
-            File projectRoot = getDevelopmentHome();
-            ModuleHistoryFactory factory = ModuleHistoryFactory.getInstance(projectRoot);
-            assertNotNull(factory);
+          File projectRoot = getDevelopmentHome();
+          ModuleHistoryFactory factory = ModuleHistoryFactory.getInstance(projectRoot);
+          assertNotNull(factory);
 
-            ModuleHistory moduleHistory = factory.getModuleHistory(DEFAULT_MODULE_1);
-            assertNotNull(moduleHistory);
+          ModuleHistory moduleHistory = factory.getModuleHistory(DEFAULT_MODULE_1);
+          assertNotNull(moduleHistory);
 
-            moduleHistory.save();
-            assertTrue(true);
-        } catch (Exception e) {
-            fail(e.getMessage());
-        }
-    }
+          ModuleHistoryEvent event = new ModuleHistoryEvent();
+
+          event.setType(ModuleHistoryEvent.CREATE_MODULE_EVENT);
+          event.setDatetime(new Date());
+          event.setAuthor("me");
+          event.setComment("new module created");
+          event.setVersion(new Version("0-0"));
+
+          moduleHistory.addEvent(event);
+          moduleHistory.save();
+
+          assertTrue(true);
+      } catch (Exception e) {
+          fail(e.getMessage());
+      }
+  }
 
 }
