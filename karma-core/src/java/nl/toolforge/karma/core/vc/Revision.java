34bd00fbf6f02ce019ac0da6a0943464d091b099
Hilbert Schraal <hilbert@hippe-it.nl>
2004-08-29 18:00:31 +0000

    license added

--- karma-core/src/java/nl/toolforge/karma/core/vc/Revision.java
+++ karma-core/src/java/nl/toolforge/karma/core/vc/Revision.java
@@ -1,10 +1,28 @@
+/*
+Karma core - Core of the Karma application
+Copyright (C) 2004  Toolforge <www.toolforge.nl>
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 package nl.toolforge.karma.core.vc;
 
 /**
  * Model interface for a revision in a version control system.
  *
  * @author D.A. Smedes
- * @version $id$
+ * @version $Id$
  */
 public interface Revision {
 }
