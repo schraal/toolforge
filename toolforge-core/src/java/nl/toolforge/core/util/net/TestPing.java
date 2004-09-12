680dc7e6c090f46d536e7d65a01ec6501c7318fd
Hilbert Schraal <hilbert@hippe-it.nl>
2004-09-12 11:45:24 +0000

    changed test so that it pings to localhost

--- toolforge-core/src/java/nl/toolforge/core/util/net/TestPing.java
+++ toolforge-core/src/java/nl/toolforge/core/util/net/TestPing.java
@@ -2,8 +2,6 @@ package nl.toolforge.core.util.net;
 
 import junit.framework.TestCase;
 
-import java.util.Date;
-
 /**
  * @author D.A. Smedes
  * @version $Id$
@@ -12,9 +10,12 @@ public class TestPing extends TestCase {
 
   public void testPing() {
 
-    // Return results within 100 ms.
+    // Return results within 1000 ms.
     //
-    assertTrue(Ping.ping("www.sourceforge.net", 80, 1000));
+    //todo: make this test work always. E.g. by starting a socket listener
+    //before starting the test. This way we can garantee that te test will work.
+    //pinging to www.sourceforge.net:80 does not work when you're behind a proxy.
+    assertTrue(Ping.ping("127.0.0.1", 22, 1000));
     
     assertFalse(Ping.ping("blaat", 80, 1000));
   }
