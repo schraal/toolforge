96bcd3818e4a0ca15e8631433206bc274f9ecdc7
Arjen Smedes <d.a.smedes@doubleforge.com>
2004-09-20 05:55:04 +0000

    Update imports.

--- karma-core/src/java/nl/toolforge/karma/core/manifest/ReleaseManifest.java
+++ karma-core/src/java/nl/toolforge/karma/core/manifest/ReleaseManifest.java
@@ -19,10 +19,10 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 package nl.toolforge.karma.core.manifest;
 
 import nl.toolforge.karma.core.KarmaRuntimeException;
-import nl.toolforge.karma.core.vc.threads.ParallelRunner;
-import nl.toolforge.karma.core.vc.cvs.threads.PatchLineThread;
 import nl.toolforge.karma.core.boot.WorkingContext;
 import nl.toolforge.karma.core.location.LocationException;
+import nl.toolforge.karma.core.vc.cvs.threads.PatchLineThread;
+import nl.toolforge.karma.core.vc.threads.ParallelRunner;
 
 import java.io.File;
 
