3c6d96e7de6ed0d37bb6830f421b7b55d40d9013
Hilbert Schraal <hilbert@hippe-it.nl>
2004-09-28 18:00:31 +0000

    big catch all around the CLI to prevent a hang after and uncought exception

--- karma-cli/src/java/nl/toolforge/karma/cli/CLI.java
+++ karma-cli/src/java/nl/toolforge/karma/cli/CLI.java
@@ -1,20 +1,17 @@
 package nl.toolforge.karma.cli;
 
+import java.text.MessageFormat;
+import java.util.prefs.Preferences;
+
+import nl.toolforge.karma.cli.cmd.CLICommandResponseHandler;
 import nl.toolforge.karma.core.boot.WorkingContext;
+import nl.toolforge.karma.core.cmd.Command;
 import nl.toolforge.karma.core.cmd.CommandContext;
 import nl.toolforge.karma.core.cmd.CommandException;
 import nl.toolforge.karma.core.cmd.CommandFactory;
 import nl.toolforge.karma.core.cmd.CommandLoadException;
-import nl.toolforge.karma.core.cmd.Command;
 import nl.toolforge.karma.core.location.LocationException;
 import nl.toolforge.karma.core.manifest.ManifestException;
-import nl.toolforge.karma.cli.cmd.CLICommandResponseHandler;
-
-import java.util.prefs.Preferences;
-import java.util.Collections;
-import java.util.Arrays;
-import java.lang.reflect.Array;
-import java.text.MessageFormat;
 
 /**
  * The command-line-interface for Karma. This class runs one command, then quits (gracefully hopefully). All arguments
@@ -27,41 +24,46 @@ public final class CLI {
 
   public static void main(String[] args) {
 
-    if (args.length == 0) {
-
-      System.out.println("\nCommand-line is missing ...\n\n");
-      System.out.println("CLI usage :\n\n");
-
-      System.out.println("karma <command-line-string>              Runs the command that is passed as a command-line-string");
-
-      System.exit(1);
-    }
-
-    CLI cli = new CLI();
-
     try {
-      cli.runCommand(args);
-    } catch (CommandException e) {
+      if (args.length == 0) {
 
-      // todo the dame ugly way ...
-      //
-      String message;
-      if (e.getMessageArguments() != null && e.getMessageArguments().length != 0) {
-        MessageFormat messageFormat = new MessageFormat(e.getErrorMessage());
-        message = messageFormat.format(e.getMessageArguments());
-      } else {
-        message = e.getErrorMessage();
+        System.out.println("\nCommand-line is missing ...\n\n");
+        System.out.println("CLI usage :\n\n");
+
+        System.out.println("karma <command-line-string>              Runs the command that is passed as a command-line-string");
+
+        System.exit(1);
       }
-      System.out.println("\n" + message);
 
-      System.out.println("\n\n\n\n");
+      CLI cli = new CLI();
 
-      e.printStackTrace();
+      try {
+        cli.runCommand(args);
+      } catch (CommandException e) {
 
-      System.exit(1);
+        // todo the dame ugly way ...
+        //
+        String message;
+        if (e.getMessageArguments() != null && e.getMessageArguments().length != 0) {
+          MessageFormat messageFormat = new MessageFormat(e.getErrorMessage());
+          message = messageFormat.format(e.getMessageArguments());
+        } else {
+          message = e.getErrorMessage();
+        }
+        System.out.println("\n" + message);
+
+        System.out.println("\n\n\n\n");
+
+        e.printStackTrace();
+
+        System.exit(1);
+      }
+
+      System.exit(0);
+    } catch (Exception e) {
+      System.out.println("-- catch all ---\n");
+      e.printStackTrace();  
     }
-
-    System.exit(0);
   }
 
   private void runCommand(String[] arguments) throws CommandException {
@@ -93,9 +95,23 @@ public final class CLI {
     try {
       commandContext.init(new CLICommandResponseHandler());
     } catch (LocationException e) {
-      System.out.println(e.getErrorMessage());
+      String message;
+      if (e.getMessageArguments() != null && e.getMessageArguments().length != 0) {
+        MessageFormat messageFormat = new MessageFormat(e.getErrorMessage());
+        message = messageFormat.format(e.getMessageArguments());
+      } else {
+        message = e.getErrorMessage();
+      }
+      System.out.println("\n" + message);
     } catch (ManifestException e) {
-      System.out.println(e.getErrorMessage());
+      String message;
+      if (e.getMessageArguments() != null && e.getMessageArguments().length != 0) {
+        MessageFormat messageFormat = new MessageFormat(e.getErrorMessage());
+        message = messageFormat.format(e.getMessageArguments());
+      } else {
+        message = e.getErrorMessage();
+      }
+      System.out.println("\n" + message);
     }
 
     commandContext.execute(commandLine);
