73af2c75d7e89432ef6a25029ccd9ce6cbb3dacf
Hilbert Schraal <hilbert@hippe-it.nl>
2004-09-28 18:01:47 +0000

    more advanced help formatting

--- karma-cli/src/java/nl/toolforge/karma/console/CommandRenderer.java
+++ karma-cli/src/java/nl/toolforge/karma/console/CommandRenderer.java
@@ -18,21 +18,22 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 package nl.toolforge.karma.console;
 
-import nl.toolforge.karma.core.cmd.Command;
-import nl.toolforge.karma.core.cmd.CommandDescriptor;
-import nl.toolforge.karma.core.cmd.CommandFactory;
-import nl.toolforge.karma.core.cmd.CommandLoadException;
-import nl.toolforge.karma.core.cmd.CommandException;
-import nl.toolforge.core.util.lang.NiftyStringUtils;
-import org.apache.commons.cli.Option;
-import org.apache.commons.lang.StringUtils;
-
 import java.util.Collection;
 import java.util.Iterator;
 
+import org.apache.commons.cli.Option;
+import org.apache.commons.lang.StringUtils;
+
+import nl.toolforge.core.util.lang.NiftyStringUtils;
+import nl.toolforge.karma.core.cmd.CommandDescriptor;
+import nl.toolforge.karma.core.cmd.CommandException;
+import nl.toolforge.karma.core.cmd.CommandFactory;
+import nl.toolforge.karma.core.cmd.CommandLoadException;
+
 public class CommandRenderer {
 
-  public static final int FILL = 50;
+  public static final int COMMAND_FILL = 40;
+  public static final int OPTION_FILL = 50;
 
   public static String commands = null;
 
@@ -55,7 +56,7 @@ public class CommandRenderer {
     Collection optionsCollection = descriptor.getOptions().getOptions();
     Option[] options = (Option[]) optionsCollection.toArray(new Option[optionsCollection.size()]);
 
-    return new StringBuffer().append(printCommand(descriptor, options, true)).toString();
+    return new StringBuffer().append(printCommand(descriptor, options, true, true)).toString();
   }
 
   /**
@@ -81,7 +82,7 @@ public class CommandRenderer {
         Collection optionsCollection = descriptor.getOptions().getOptions();
         Option[] options = (Option[]) optionsCollection.toArray(new Option[optionsCollection.size()]);
 
-        buffer.append(printCommand(descriptor, options, false));
+        buffer.append(printCommand(descriptor, options, false, false));
       }
       commands = buffer.toString();
     }
@@ -89,7 +90,7 @@ public class CommandRenderer {
     return commands;
   }
 
-  private static StringBuffer printCommand(CommandDescriptor descriptor, Option[] options,  boolean showHelp) {
+  private static StringBuffer printCommand(CommandDescriptor descriptor, Option[] options, boolean showOptions, boolean showHelp) {
 
     StringBuffer buffer = new StringBuffer();
 
@@ -99,37 +100,44 @@ public class CommandRenderer {
 
     // Command name + alias
     //
-    buffer.append("\n");
+//    buffer.append("\n");
+    String commandNameAlias = descriptor.getName()+" ("+descriptor.getAlias()+")";
+
     buffer.
-        append(descriptor.getName()).
-        append(" (").append(descriptor.getAlias()).append(")").
-        append("\n");
+        append( commandNameAlias ).
+        append( StringUtils.repeat(" ", COMMAND_FILL - commandNameAlias.length()) );
+    if (!showOptions) {
+      buffer.append( descriptor.getDescription() );
+    }
+    buffer.append( "\n" );
 
-    for (int j = 0; j < options.length; j++) {
+    if (showOptions) {
+      for (int j = 0; j < options.length; j++) {
 
-      Option o = options[j];
+        Option o = options[j];
 
-      String leftPadding = "   ";
+        String leftPadding = "   ";
 
-      buffer.
-          append(leftPadding).
-          append("-" + o.getOpt()).
-          append(", --" + o.getLongOpt());
+        buffer.
+            append(leftPadding).
+            append("-" + o.getOpt()).
+            append(", --" + o.getLongOpt());
 
-      String args = "";
-      if (o.hasArg()) {
-        args = " <".concat(o.getArgName()).concat(">");
+        String args = "";
+        if (o.hasArg()) {
+          args = " <".concat(o.getArgName()).concat(">");
+        }
+
+        // todo when the commands are described with too much of text, then FILL will run out of count ...
+        //
+        buffer.append(args.concat(StringUtils.repeat(" ", OPTION_FILL - (o.getLongOpt() + args).length())));
+        buffer.append(leftPadding);
+        if (!o.isRequired()) {
+          buffer.append("(Optional) ");
+        }
+        buffer.append(o.getDescription());
+        buffer.append("\n");
       }
-
-      // todo when the commands are described with too much of text, then FILL will run out of count ...
-      //
-      buffer.append(args.concat(StringUtils.repeat(" ", FILL - (o.getLongOpt() + args).length())));
-      buffer.append(leftPadding);
-      if (!o.isRequired()) {
-        buffer.append("(Optional) ");
-      }
-      buffer.append(o.getDescription());
-      buffer.append("\n");
     }
 
     if (showHelp) {
